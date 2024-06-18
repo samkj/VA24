6ceece93446b33859c76561cf96214d6c3a03cee
steromen <152521030+steromen@users.noreply.github.com>
2024-06-18 15:31:54 +0200

    code documentation

--- scripts/BERT_Sentiment.py
+++ scripts/BERT_Sentiment.py
@@ -4,20 +4,31 @@ from tqdm import tqdm
 from pathlib import Path
 
 def analyze_sentiments(input_csv, output_csv, state_output_dir, model_name="ssary/XLM-RoBERTa-German-sentiment", max_token_length=512):
+    """
+    analyze the sentiment on subreddit comments about austrian parties, 
+    for each state-subreddit where comments about the austrian parties were found (ÖVP, FPÖ, SPÖ, Grüne and Neos),
+    save results in a CSV file
+    """
+    
+    #initialize sentiment analysis pipeline with the "ssary/XLM-RoBERTa-German-sentiment" model
     sentiment_pipeline = pipeline(model=model_name)
     
+    #read reddit data
     Reddit_data = pd.read_csv(input_csv)
 
+    #initialize new columns
     Reddit_data['BERT_class'] = "Neutral"
     Reddit_data['BERT_Compound'] = 0.0
     Reddit_data['BERT_label'] = 0.0
-
+    
+    #create sentiment analysis on each comment
     for i in tqdm(range(len(Reddit_data['comment_body'])), desc="Processing Sentiments"):
         comment = Reddit_data['comment_body'][i][:max_token_length] 
         sentiment_dict = sentiment_pipeline(comment)
         Reddit_data.loc[i, 'BERT_class'] = sentiment_dict[0]['label'].upper()
         Reddit_data.loc[i, 'BERT_Compound'] = sentiment_dict[0]['score']
         
+        #calculate the BERT_label on the basis of BERT_class and BERT_Compound
         if Reddit_data.loc[i, 'BERT_class'] == 'POSITIVE':
             Reddit_data.loc[i, 'BERT_label'] = 3.0 - Reddit_data.loc[i, 'BERT_Compound']
         elif Reddit_data.loc[i, 'BERT_class'] == 'NEUTRAL':
@@ -25,6 +36,7 @@ def analyze_sentiments(input_csv, output_csv, state_output_dir, model_name="ssar
         elif Reddit_data.loc[i, 'BERT_class'] == 'NEGATIVE':
             Reddit_data.loc[i, 'BERT_label'] = 1.0 - float(Reddit_data.loc[i, 'BERT_Compound'])
 
+    #filter out rows where the BERT_Compound is zero
     Reddit_data = Reddit_data[Reddit_data['BERT_Compound'] != 0.0]
     
     # Save the result to the output CSV file
@@ -36,16 +48,20 @@ def analyze_sentiments(input_csv, output_csv, state_output_dir, model_name="ssar
         state_output_file = Path(state_output_dir) / f"{state.lower()}_politik_posts_sentiment.csv"
         group.to_csv(state_output_file, index=False)
 
+    #read output CSV file for further analysis
     data = pd.read_csv(output_csv)
 
+    #calculate sentiment percentage
     sentiment_percentage = data["BERT_class"].value_counts(normalize=True) * 100
     print("Sentiment Percentage:")
     print(sentiment_percentage)
 
+    #calculate mean sentiment
     mean_sentiments = data.groupby('BERT_class')['BERT_label'].mean()
     print("Mean Sentiments:")
     print(mean_sentiments)
 
+#function call
 analyze_sentiments(
     input_csv='subreddits_datafiles/processed_datafiles/cleaned_data1.csv', 
     output_csv='subreddits_datafiles/processed_datafiles_sentiment/sentiment_all_subreddits_data.csv',
--- scripts/Reddit_data.py
+++ scripts/Reddit_data.py
@@ -6,12 +6,20 @@ from praw.models import Submission
 from datetime import datetime, timezone
 
 def get_reddit_data(subreddits, keywords, limit, csv_file_path, start_date, end_date):
+    """
+    scrapping reddit posts and comments from austrian subreddits on the basis of 
+    austrian parties (ÖVP, FPÖ, SPÖ, Grüne, Neos) as keywords and a date range 
+    (from last election in 2019 till end of April 2024) and save the data into a CSV file.
+    """
+
+    #reddit API credentials
     REDDIT_ID ="doVr0Tzy7XzxNX49V6WEBA"
     REDDIT_SECRET = "meyYVEetTi32wnbgKs8FNiKwgjDA-Q"
     USER_AGENT = "visual_analytics_groupproject"
     USERNAME = "Great-Oil5596"
     PASSWORD = "G2QxAiqNTMq$84n"
 
+    #initialize reddit instance
     reddit = praw.Reddit(
         client_id=REDDIT_ID,
         client_secret=REDDIT_SECRET,
@@ -22,17 +30,24 @@ def get_reddit_data(subreddits, keywords, limit, csv_file_path, start_date, end_
 
     all_results = []
 
+    #convert start_date and end_date to datetime objects
     start_date = datetime.strptime(start_date, "%Y-%m-%d").replace(tzinfo=timezone.utc)
     end_date = datetime.strptime(end_date, "%Y-%m-%d").replace(tzinfo=timezone.utc)
 
+    #iterate through each subreddit
     for sub in tqdm(subreddits):
         subreddit = reddit.subreddit(sub)
+        
+        #iterate through each keyword
         for keyword in keywords:
             results = []
             selected_threads = subreddit.search(keyword, limit=limit)
-
+            
+            #iterate through each post
             for post in selected_threads:
                 post_date = datetime.fromtimestamp(post.created_utc, timezone.utc)
+                
+                #check post date if it is within the specified range of start_date and end_date
                 if start_date <= post_date <= end_date:
                     data = {
                         'author_name': post.author.name if post.author else 'No author',
@@ -43,11 +58,15 @@ def get_reddit_data(subreddits, keywords, limit, csv_file_path, start_date, end_
                         'keyword': keyword,
                     }
 
+                    #scrapping all comments for the post
                     post.comments.replace_more(limit=None)
                     comments = post.comments.list()
 
+                    #iterate through each comment
                     for comment in comments:
                         comment_created_utc = datetime.fromtimestamp(comment.created_utc, timezone.utc)
+                        
+                        #check comment date if it is within the specified range and if it contains the keyword
                         if start_date <= comment_created_utc <= end_date and keyword.lower() in comment.body.lower():
                             comment_data = {
                                 'comment_author_name': comment.author.name if comment.author else 'No author',
@@ -64,14 +83,13 @@ def get_reddit_data(subreddits, keywords, limit, csv_file_path, start_date, end_
 
             all_results.extend(results)
 
+    #write results to a CSV file
     with open(csv_file_path, "w", newline='', encoding='utf-8') as csvfile:
         fieldnames = ['author_name', 'post_id', 'title', 'body', 'post_date', 'keyword', 'comment_author_name', 'comment_id', 'comment_parent_id', 'comment_body', 'comment_score', 'comment_created_utc', 'comment_replies_count', 'comment_keyword', 'subreddit']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(all_results)
 
-#all subreddits
-#get_reddit_data(["Austria", "europe", "Stmk", "wien", "Tirol", "Salzburg", "Vorarlberg", "kaernten", "burgenland", "Noesterreich", "Linz"], ["ÖVP", "SPÖ", "FPÖ", "Grüne", "Neos"], 100, "C:/Stefan/Uni Graz/Master/VU Visual Analytics/austria_politik_posts2.csv", "2019-09-29", "2024-04-29")
-
+#example function usage:
 get_reddit_data(["Linz"], ["ÖVP", "SPÖ", "FPÖ", "Grüne", "Neos"], None, "subreddits_datafiles\Linz_politik_posts.csv", "2019-09-29", "2024-04-29")
 
